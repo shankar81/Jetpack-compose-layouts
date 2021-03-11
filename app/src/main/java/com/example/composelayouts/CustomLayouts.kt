51c139921f9f0c718514dd302565b80c46492e4d
shankar81 <shankarsawant81@gmail.com>
2021-03-11 08:29:28 +0530

    Final code of staggered grid

--- app/src/main/java/com/example/composelayouts/CustomLayouts.kt
+++ app/src/main/java/com/example/composelayouts/CustomLayouts.kt
@@ -3,14 +3,29 @@ package com.example.composelayouts
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+import com.google.android.material.chip.Chip
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
 
 class CustomLayouts : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,21 +34,96 @@ class CustomLayouts : AppCompatActivity() {
             ComposeLayoutsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-
+                    Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+                        StaggeredGrid {
+                            for (topic in topics) {
+                                Chip(Modifier.padding(8.dp), text = topic)
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-//@Composable
-//fun StaggeredGrid(
-//    modifier: Modifier = Modifier,
-//    rows: Int = 3, content:
-//    @Composable () -> Unit
-//) {
-//    Layout(modifier = modifier, content = content) { measurables, constraints ->
-//
-//    }
-//}
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier,
+        border = BorderStroke(Dp.Hairline, Color.Black),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+
+        // Keep track of width of each rows
+        val rowWidths = IntArray(rows) { 0 }
+
+        // Keep track of maxHeight of each rows
+        val rowMaxHeights = IntArray(rows) { 0 }
+
+        // Don't constraint child view further.
+        // List of measured children
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowMaxHeights[row] = Math.max(rowMaxHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // Grids width is widest row
+        val width =
+            rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+
+        // Grids height is sum of tallest elements of each row
+        val height = rowMaxHeights.sumBy { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // Y of each row
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowMaxHeights[i - 1]
+        }
+
+        // Set the size of the parent layout
+        layout(width, height) {
+            // X cord
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(x = rowX[row], y = rowY[row])
+
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
 
